bb73278c2a0b684088869697ad9c9a4165492c9c
Craig Labenz <craig.labenz@gmail.com>
2025-11-13 10:27:54 -0800

    local or prod dart frog + hot reloadable flutter web setup

--- webproxy/app/lib/main.dart
+++ webproxy/app/lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const API_BASE_URL = String.fromEnvironment('API_BASE_URL');
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    final rawUrl = '$API_BASE_URL/api/users';
+    print('rawUrl: $rawUrl');
+    http.get(Uri.parse(rawUrl)).then((response) {
+      print(response.body);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello Solar System!'))),
     );
   }
 }
